9af537082c533f5e1ce3ec8b5f9bfa0830b07061
decka <decka@github.com>
2018-05-25 14:01:09 +0200

    now

--- javascript.js
+++ javascript.js
@@ -20,11 +20,11 @@ let urlParams= new URLSearchParams(window.location.search);
 
 let categories = urlParams.get("categories");
 console.log("I want to get the category: " + categories);
-
+function fetchMusic(){
 fetch("http://deckadev.com/wordpress/wp-json/wp/v2/shop?categories=" + categories)
 .then(e=>e.json())
 .then(showProductType)
-
+}
 
 
 function showProductType(data){
@@ -35,14 +35,45 @@ function showProduct(aProduct){
     console.log(aProduct);
     let template = document.querySelector("#template").content;
     let clone = template.cloneNode(true);
- clone.querySelector(".more").href="subpage_music.html?id=" + aProduct.id;
+ clone.querySelector(".more").href="subpage.html?id=" + aProduct.id;
  clone.querySelector(".name-product").textContent = aProduct.title.rendered;
  clone.querySelector(".img-post").src = aProduct.acf.image.sizes.medium;
+clone.querySelector(".number").textContent = aProduct.acf.price + " DKK";
 
+    if (aProduct.acf.availability == false){
+       clone.querySelector(".item").classList.add('opacity');
+        clone.querySelector(".sold_out").innerHTML = "SOLD OUT";
+    };
 
     let category = document.querySelector("#category");
     category.appendChild(clone);
 
 }
+fetchMusic();
 
+
+
+let id = urlParams.get("id");
+console.log("I want to get article: " + id);
+
+fetch("http://deckadev.com/wordpress/wp-json/wp/v2/shop/" + id)
+.then(e=>e.json())
+.then(showSinglePost)
+
+function showSinglePost(anItem){
+    console.log(anItem);
+
+
+ document.querySelector(".name-product").textContent = anItem.title.rendered;
+
+document.querySelector(".desc").innerHTML = anItem.content.rendered;
+
+document.querySelector(".img-post").src = anItem.acf.image.sizes.medium;
+
+    document.querySelector(".number").textContent = anItem.acf.price+ " DKK";
+    document.querySelector(".size").textContent = "SIZE: " + anItem.acf.size;
+    if (anItem.acf.availability == false){
+        document.querySelector(".sold_out").innerHTML = "SOLD OUT";
+    };
+}
 
